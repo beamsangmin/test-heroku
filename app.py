863e5f38a8e5c95183acb6e069c100df2ed6c9fd
MrBeam <Beamsangmin@gmail.com>
2022-08-04 16:58:26 +0700

    xxx

--- app.py
+++ app.py
@@ -71,6 +71,9 @@ def callback():
 
 def reply(intent,text,reply_token,id,disname):
     print(intent)
+    text_message = StickerMessage(package_id=8525,sticker_id=16581292)
+    line_bot_api.reply_message(reply_token,text_message)
+
     if intent == 'Vendor':
         text_message = TextSendMessage(text='สวัสดี Vendor '+disname)
         line_bot_api.reply_message(reply_token,text_message)
