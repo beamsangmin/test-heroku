f3b65736e5c24bbea37e4d3fca238d2dc57079fa
MrBeam <Beamsangmin@gmail.com>
2022-08-04 15:18:53 +0700

    xxx

--- app.py
+++ app.py
@@ -9,7 +9,11 @@ app = Flask(__name__)
 # Channel access token and Channel secret 
 line_bot_api = LineBotApi('KtzrTuJet6PYQfzQRQEnV6F6QrC8VQTH+hzLKTfpj99SxRp1vG00aidjuAHU/YLayESkb22eatO0SU/YoYxSYbpK1bEQOUITQ7o8M2yR2phcLSsiwsJxd6dnXp4s77eB1RFC1r/6nzedhhQb1uQkXwdB04t89/1O/w1cDnyilFU=')
 handler = WebhookHandler('277fb1c0412709857645ac19242f7be7')
-      
+
+@app.route("/", methods=['GET'])
+def helloworld():
+    return 'helloworld'
+        
 @app.route("/callback", methods=['POST'])
 def callback():
     body = request.get_data(as_text=True)
