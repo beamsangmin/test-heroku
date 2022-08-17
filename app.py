f047f5187487fa4e3e3c86fef1fd64ef32cdfe4d
MrBeam <Beamsangmin@gmail.com>
2022-08-17 13:14:17 +0700

    xxx

--- app.py
+++ app.py
@@ -159,7 +159,7 @@ def replyKIN(inputmessage,retmessage,reply_token,id,disname):
         for data in json_result['value']:
                 Display = Display + (data['itemName']) + '\n'
                 
-        text_message = TextSendMessage(text="รายชื่อบริษัท : " + Display + '\n' + retmessage)
+        text_message = Display[0:5000] # TextSendMessage(text="รายชื่อบริษัท : " + Display + '\n' + retmessage)
         line_bot_api.reply_message(reply_token,text_message)
     
 if __name__ == '__main__': app.run(debug=True)
